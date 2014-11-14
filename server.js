be53ac8e901ec2fee68188401eee35846671033a
Stas Ukolov <ukoloff@gmail.com>
2014-11-14 16:02:38 +0500

    Immediately flush headers

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ function Server(req, res)
   res.setHeader('Content-Type', 'text/plain')
   res.setHeader('X-Ticket', 12345)
   res.writeHead(200)
-  res.write('.')
+
+  // Write the headers to the socker
+  res.socket.write(res._header);
+  // Mark the headers as sent
+  res._headerSent = true;
+
   setTimeout(function()
   {
     res.end('Hello World\n')
