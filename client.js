df23778e5ab2409363768a12123763c70fcf3f2f
Stas Ukolov <ukoloff@gmail.com>
2014-11-21 14:36:07 +0500

    Optimize client

--- client.js
+++ client.js
@@ -1,25 +1,9 @@
 var
-  websocket = require('websocket-stream'),
-  ws = require('ws')
+  ws = require('ws'),
   client = new ws('ws://localhost:4567/ssh')
 
-client.on('open', Open)
-
-function Open()
+client.on('open', function()
 {
-  console.log('OPEN')
-  // var z = websocket(client)
-  process.stdin.on('readable', Read)
-  client.on('message', Msg)
-}
-
-function Read()
-{
-  console.log('SEND')
-  client.send(this.read())
-}
-
-function Msg(data)
-{
-  process.stdout.write(data)
-}
+  process.stdin.on('readable', function(){ client.send(this.read()) })
+  client.on('message', function(data){ process.stdout.write(data) })
+})
