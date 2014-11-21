dc2d7c18c117d8c2890460f0cae9ead3e0cbe893
Stas Ukolov <ukoloff@gmail.com>
2014-11-21 11:11:29 +0500

    Hand made stream

--- client.js
+++ client.js
@@ -1,7 +1,25 @@
 var
   websocket = require('websocket-stream'),
-  ws = websocket('ws://localhost:4567/ssh')
+  ws = require('ws')
+  client = new ws('ws://localhost:4567/ssh')
 
-process.stdin
-.pipe(ws)
-.pipe(process.stdout)
+client.on('open', Open)
+
+function Open()
+{
+  console.log('OPEN')
+  // var z = websocket(client)
+  process.stdin.on('readable', Read)
+  client.on('message', Msg)
+}
+
+function Read()
+{
+  console.log('SEND')
+  client.send(this.read())
+}
+
+function Msg(data)
+{
+  process.stdout.write(data)
+}
