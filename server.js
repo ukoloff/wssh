3d3b4beabbf90c472dcef2d64aa1ccd471f4a8d0
Stas Ukolov <ukoloff@gmail.com>
2014-11-21 14:57:20 +0500

    Pipe to socket

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 var
+  net = require('net')
   ws = require('ws').Server,
   websocket = require('websocket-stream'),
-  responder = require('./responder'),
-  wss = new ws({port: 4567})
 
-wss.on('connection', function(ws)
+new ws({port: 4567})
+.on('connection', function(ws)
 {
-  ws = websocket(ws)
-  ws.pipe(responder()).pipe(ws)
+  var
+    ssh = net.connect(22, '10.220.6.10')
+  ssh
+  .on('error', Err)
+  .on('connect', Start)
+
+  function Err()
+  {
+    ws.close()
+  }
+
+  function Start()
+  {
+    this.pipe(websocket(ws)).pipe(this)
+  }
 })
