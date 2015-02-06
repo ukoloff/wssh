bcd2ff1a01d8ea1454cea70a8e1f46e796fbe37b
Stas Ukolov <ukoloff@gmail.com>
2015-02-06 11:45:53 +0500

    Start server side dispatching

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
 var
+  fs = require('fs'),
   net = require('net')
   ws = require('ws').Server,
   websocket = require('websocket-stream'),
+  yaml = require('js-yaml')
 
 new ws({port: 4567})
 .on('connection', function(ws)
 {
-  var
-    ssh = net.connect(22, '10.220.6.10')
-  ssh
+  fs.readFile(__dirname+'/hosts.yml', Hosts)
+  net.connect(22, '10.220.6.10')
   .on('error', Err)
   .on('connect', Start)
+
+  function Hosts(err, yml)
+  {
+    if(err)
+    {
+      ws.send('Oops: '+err)
+      ws.close()
+    }
+    try
+    {
+      var hosts = yaml.safeLoad(yml)
+      console.log('Connect', ws.upgradeReq.url)
+    }
+    catch(e)
+    {
+      ws.send('Oops: '+e)
+      ws.close()
+    }
+  }
 
   function Err()
   {
